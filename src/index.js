246014121dccb1e8ba29a05feecb4d9178bdf028
mac <mac@macs-MacBook-Pro.local>
2023-02-27 23:30:29 +0300

    feat: ended reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = n + '';
+    let reversedText = n.split('').reverse().join('');
+    let reversed = parseInt(reversedText, 10);
+    return reversed
+
 }
+
